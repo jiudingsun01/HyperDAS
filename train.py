1919bc4a014f93ebddaf2b5e87f7a4b3496185cc
Jiuding Sun <sjd24@sphinx5.stanford.edu>
2024-12-09 23:55:07 -0800

    save before checkout

--- train.py
+++ train.py
@@ -151,13 +151,13 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--seed", type=int, default=42)
     parser.add_argument("--log_wandb", type=bool, default=True)
-    parser.add_argument("--wandb_project", type=str, default="HyperDAS-Symmetry")
+    parser.add_argument("--wandb_project", type=str, default="HyperDAS-Symmetry-Real")
     parser.add_argument("--wandb_run_name", type=str, default="City-HouseHolder")
     parser.add_argument("--intervention_layer", type=int, default=15)
     
     parser.add_argument("--load_trained_from", type=str, default=None)
     
-    parser.add_argument("--n_epochs", type=int, default=5)
+    parser.add_argument("--n_epochs", type=int, default=3)
     parser.add_argument("--model_name_or_path", type=str, default="/nlp/scr/sjd24/llama3-8b")
     parser.add_argument("--batch_size", type=int, default=16)
     parser.add_argument("--source_suffix_visibility", default=False, action="store_true")
@@ -165,16 +165,16 @@ if __name__ == "__main__":
 
     parser.add_argument("--bos_token_visibility", default=False)
     
-    parser.add_argument("--test_path", type=str, default="./experiments/RAVEL/data/city_test_small")
-    parser.add_argument("--train_path", type=str, default="./experiments/RAVEL/data/city_train")
+    parser.add_argument("--test_path", type=str, default="./experiments/RAVEL/data/test_cleaned")
+    parser.add_argument("--train_path", type=str, default="./experiments/RAVEL/data/train_large_cleaned")
      
     parser.add_argument("--causal_loss_weight", type=float, default=3.5)
-    parser.add_argument("--iso_loss_weight", type=float, default=1)
+    parser.add_argument("--iso_loss_weight", type=float, default=0)
     
-    parser.add_argument("--save_dir", type=str, default="/nlp/scr/sjd24/city_symmetry")
+    parser.add_argument("--save_dir", type=str, default="/scr-ssd/sjd24/city_symmetry_real")
     parser.add_argument("--save_model", default=False, action="store_true")
     
-    parser.add_argument("--num_decoders", type=int, default=4)
+    parser.add_argument("--num_decoders", type=int, default=2)
     parser.add_argument("--initialize_from_scratch", default=False, action="store_true")
     
     # Sparsity Loss
@@ -184,11 +184,11 @@ if __name__ == "__main__":
     # if None, use Boundless DAS
     parser.add_argument('--subspace_module', default="ReflectSelect", choices=[None, "DAS", "BoundlessDAS", "MaskSelect", "ReflectSelect", "QuasiProjective"])
     parser.add_argument("--das_dimension", type=int, default=128)
-    parser.add_argument("--lr", type=float, default=2e-5)
+    parser.add_argument("--lr", type=float, default=2e-4)
     parser.add_argument("--weight_decay", type=float, default=0.01)
     
     parser.add_argument("--eval_per_steps", type=int, default=1000)
-    parser.add_argument("--checkpoint_per_steps", type=int, default=None)
+    parser.add_argument("--checkpoint_per_steps", type=int, default=5000)
     
     args = parser.parse_args()
     args = dict(args.__dict__)
